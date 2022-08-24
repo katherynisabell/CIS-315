83aaf79a26dd7870a5b44075574861dd73d37483
katherynisabell <111913185+katherynisabell@users.noreply.github.com>
2022-08-24 12:35:17 -0500

    Add files via upload

---
+++ MyFirstCalculations.java
@@ -0,0 +1,26 @@
+public class MyFirstCalculations {
+    public static void main(String args[]) {
+        
+// Declare and initialize variables
+    int n1 = 20;
+    int n2 = 10;
+    double n3 = 25.2;
+
+// Process calculations
+    int sum = n1 + n2; int
+    minus = n1 - n2; int
+    multiply = n1 * n2; int
+    divide = n1 / n2;
+    int remainder = n1 % n2;
+    double div1 = n3 / n2;  
+    double div2 = n1 / n3;
+// Display output
+     System.out.println("Sum is: " + sum);
+     System.out.println("Minus value is: " + minus);
+     System.out.println("Multiplication: " + multiply);
+     System.out.println("Divide: " + divide);
+     System.out.println("Remainder: " + remainder);
+     System.out.println("Divide 1: " + div1);
+     System.out.println("Divide 2: " + div2);
+ }
+} 
