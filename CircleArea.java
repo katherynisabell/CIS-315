62831d72269fd4d4c86e4d753c2b25d707eaca83
katherynisabell <111913185+katherynisabell@users.noreply.github.com>
2022-08-29 12:27:09 -0500

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,19 @@
+// A Java program to find the area of a circle
+
+public class CircleArea {
+    public static void main(String args[]) {
+        // Set up the variables
+        
+        double radius;
+        double area; 
+        radius = 20;
+        
+        // Calculation
+        
+        area = 3.14159 * radius;
+        
+        // Display the Result
+
+      System.out.println("The area of a circle with a radius "+ radius + " is " + area);
+    }
+}
