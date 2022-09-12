0706e8fa78f30991da3da7326e2ea67084f60d7b
katherynisabell <111913185+katherynisabell@users.noreply.github.com>
2022-09-12 12:32:36 -0500

    Add files via upload

---
+++ TestGrade (1).java
@@ -0,0 +1,35 @@
+// Using the if-else-if statement decision syntax to figure out the letter grade for a test 
+
+// Import scanner
+import java.util.Scanner;
+
+public class TestGrade {
+    public static void main(String args[]) {
+        int testScore; 
+        
+// Create the new scanner object    
+    Scanner input = new Scanner(System.in);
+    
+//Prompt user for input
+    System.out.print("Enter your test grade: ");
+    testScore = input.nextInt();
+    
+//Define if-elese-if conditions
+    
+    if (testScore < 60 ){
+      System.out.println("Your grade is a F");
+    }
+    else if (testScore < 70 ){
+      System.out.println("Your grade is a D");
+    }
+    else if (testScore < 80 ){ 
+      System.out.println("Your grade is a C");
+    }
+    else if (testScore < 90){
+      System.out.println("Your grade is a B");    
+    }  
+    else {
+      System.out.println("Your grade is an A");        
+    }
+    }
+}
