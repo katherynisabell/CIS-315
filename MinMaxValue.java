ca60b1d823dd17cfe63754482fa9fd5c1fa11c3b
katherynisabell <111913185+katherynisabell@users.noreply.github.com>
2022-10-17 12:52:55 -0500

    Add files via upload

---
+++ MinMaxValue.java
@@ -0,0 +1,31 @@
+public class MinMaxValue {
+    public static void main(String args[]) {
+      int a = 10;
+      int b = 25;
+      
+      int c = max(a,b);
+      System.out.println("The maximum value is " + c);
+      //end main
+    }
+      
+      public static int max(int a, int b){
+        int result; 
+          
+        if(a>b)
+         result = a;
+        else
+         result = b;
+            
+        return result;
+      }
+        public static int min(int a, int b){
+        int result; 
+          
+        if(a<b)
+         result = a;
+        else
+         result = b;
+            
+        return result;
+    }
+}
